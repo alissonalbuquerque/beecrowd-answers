6e6528cd2986080fc689571e7c9ccf08090eca78
Alisson Albuquerque <alissonalbuquerque00@gmail.com>
2019-12-14 18:32:25 -0200

    add question

---
+++ iniciante/uri_1041.c
@@ -0,0 +1,48 @@
+
+#include <stdio.h>
+
+int main() {
+
+    double x, y;
+    scanf("%lf %lf", &x, &y);
+
+    if( (x > 0 || x < 0) && y == 0) {
+        printf("Eixo X\n");
+    } else if(x == 0 && (y > 0 || y < 0) ) {
+        printf("Eixo Y\n");
+    } else if(x > 0 && y > 0) {
+        printf("Q1\n");
+    } else if(x < 0 && y > 0) {
+        printf("Q2\n");
+    } else if(x < 0 && y < 0) {
+        printf("Q3\n");
+    } else if(x > 0 && y < 0) {
+        printf("Q4\n");
+    } else {
+        printf("Origem\n");
+    }
+
+
+    return 0;
+}
+
+/*
+    ----------------------------
+    Entrada:
+        4.5 -2.2
+    Saida:
+        Q4
+    ----------------------------
+    ----------------------------
+    Entrada:    
+        0.1 0.1
+    Saida:
+        Q1
+    ----------------------------
+    ----------------------------
+    Entrada:     
+        0.0 0.0
+    Saida:	
+        Origem
+    ----------------------------
+*/
