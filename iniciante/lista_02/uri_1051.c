9503cf8bc5af9aca28c2b38a15db878ae5903533
Alisson Albuquerque <alissonalbuquerque00@gmail.com>
2020-01-31 23:25:13 -0300

    add question

---
+++ iniciante/lista_02/uri_1051.c
@@ -0,0 +1,66 @@
+
+#include <stdio.h>
+
+void calcular_imposto(double* salario, double* imposto, double desconto, double taxa) {
+
+    *imposto += (*salario - desconto) * taxa;
+    *salario = *salario - (*salario - desconto);
+
+}
+
+int main() {
+
+    double salario, imposto = 0;
+    scanf("%lf", &salario);
+
+    while( salario > 0 ) {
+
+        if( salario <= 2000 ) {
+
+            imposto = imposto + 0;
+            break;
+
+        } else if( salario > 2000 && salario <= 3000 ) {
+
+            calcular_imposto(&salario, &imposto, 2000, 0.08);
+
+        } else if( salario > 3000 && salario <= 4500 ) {
+
+            calcular_imposto(&salario, &imposto, 3000, 0.18);
+
+        } else {
+
+            calcular_imposto(&salario, &imposto, 4500, 0.28);
+        }
+
+    }
+
+    if( imposto == 0 ) {
+        printf("Isento\n");
+    } else {
+        printf("R$ %.2f\n", imposto);
+    }
+
+    return 0;
+}
+
+/*
+    ----------------------------
+    Entrada:
+        3002.00
+    Saida:
+        R$ 80.36
+    ----------------------------
+    ----------------------------
+    Entrada:
+        1701.12
+    Saida:
+        Isento
+    ----------------------------
+    ----------------------------
+    Entrada:
+        4520.00
+    Saida:
+        R$ 355.60
+    ----------------------------
+*/
