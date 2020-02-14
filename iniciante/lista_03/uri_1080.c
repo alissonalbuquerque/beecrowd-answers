cb4dd63fb17e0d18b8c0d3e7c67c60040e1d11ba
Alisson Albuquerque <alissonalbuquerque00@gmail.com>
2020-02-14 15:27:46 -0300

    add question

---
+++ iniciante/lista_03/uri_1080.c
@@ -0,0 +1,55 @@
+
+#include <stdio.h>
+
+int busca(int* vetor, size_t len) {
+
+    int aux = 0, index = 0;
+
+    for(int i = 0; i < len; i++) {
+
+        if(vetor[i] > aux) {
+
+            aux = vetor[i];
+            index = i;
+
+        }
+    }
+
+    return index;
+
+}
+
+int main() {
+
+    int vetor[100];
+    int index;
+    size_t len = sizeof(vetor) / sizeof(int);;
+
+    for(int i = 0; i < len; i++) {
+        scanf("%d", &vetor[i]);
+    }
+
+    index = busca(vetor, len);
+
+
+    printf("%d\n%d\n", vetor[index], index+1);
+
+    return 0;
+}
+
+/*
+    ----------------------------
+    Entrada:
+        2
+        113
+        45
+        34565
+        6
+        ...
+        8
+
+    Saida:
+        34565
+        4
+    ----------------------------
+*/
