3811a0421322720ec809cd352899c62d68f6d252
Alisson Albuquerque <alissonalbuquerque00@gmail.com>
2020-02-15 17:57:19 -0300

    add question

--- iniciante/lista_01/uri_1012.c
+++ iniciante/lista_01/uri_1012.c
@@ -33,28 +33,3 @@ int main() {
 
     return 0;
 }
-
-/*
-    ----------------------------
-    Entrada:
-        3.0 4.0 5.2
-    Saida:
-        TRIANGULO: 7.800
-        CIRCULO: 84.949
-        TRAPEZIO: 18.200
-        QUADRADO: 16.000
-        RETANGULO: 12.000
-
-    ----------------------------
-    ----------------------------
-    Entrada:    
-        12.7 10.4 15.2
-    Saida:	
-        TRIANGULO: 96.520
-        CIRCULO: 725.833
-        TRAPEZIO: 175.560
-        QUADRADO: 108.160
-        RETANGULO: 132.080
-
-    ----------------------------
-*/
