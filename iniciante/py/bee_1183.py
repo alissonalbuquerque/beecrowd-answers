c33f486d683c80f0221d684420144ebdd8671e53
alissonalbuquerque <alissonalbuquerque.dev@gmail.com>
2024-01-27 18:56:25 -0300

    add bee 1183

---
+++ iniciante/py/bee_1183.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+
+matriz = [
+    [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+    [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+    [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+    [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+    [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+    [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+    [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+    [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+    [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+    [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+    [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+    [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+]
+
+def sum_above_diagonal(matriz):
+    
+    reduce = 0.0
+
+    for i in range(0, 11):
+        array = matriz[i][i+1:12]
+        for j in range(0, len(array)):
+            reduce += array[j]
+
+    return reduce
+
+operator = input()
+
+for i in range(0, len(matriz)):
+    for j in range(0, len(matriz[i])):
+        matriz[i][j] = float(input())
+        
+match operator :
+    case "S":
+
+        sum_diagonal = sum_above_diagonal(matriz)
+
+        print("{:.1f}".format(sum_diagonal))
+
+    case "M":
+
+        divider = 66.0
+        div_diagonal = sum_above_diagonal(matriz) / divider
+
+        print("{:.1f}".format(div_diagonal))
+
+    case _ :
+        print('')
